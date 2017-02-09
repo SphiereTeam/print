d4c055fcaef62fa5c1099e1c7d29aac62195189c
githubUserNo123 <alif.numan@sphiere.com.bn>
2017-02-09 16:39:36 +0800

    Add flyer calculations for material cost

--- js/product-calculations.js
+++ js/product-calculations.js
@@ -16,9 +16,9 @@ function getFlyerPaperType(){
 	return result;
 }
 
-function getFlyerPaperSide(){
-	var result = $('select[id=flyer-paper-side]').val();
-	console.log("Paper Side: " + result);
+function getFlyerPaperColor(){
+	var result = $('select[id=flyer-paper-color]').val();
+	console.log("Paper Color: " + result);
 
 	return result;
 }
@@ -31,63 +31,60 @@ function getFlyerQuantity(){
 
 }
 
+function getMaterialsCost(){
+
+	//get paper cuts from paper size
+	var paperSize = getFlyerPaperSize();
+	var paperCuts = 0;
+
+	if ( paperSize == "a4" ) {
+		paperCuts = 8;
+	}else if( paperSize == "a5" ){
+		paperCuts = 16;
+	}
+
+	//get qty
+	var paperQty = getFlyerQuantity();
+
+	//get paper pricing per ream from paper type
+	var paperType = getFlyerPaperType();
+	var paperPricingPerReam = 0;
+
+	if ( paperType == "mattart" ) {
+		paperPricingPerReam = 68;
+	}else if( paperType == "simili" ){
+		paperPricingPerReam = 40;
+	}
+
+	//get qty sheets
+	//qty sheets = qty/cuts
+	var qtySheets = paperQty/paperCuts;
+
+	//get qty reams per packets
+	//qty reams per packets = qty sheets/500
+	var qtyReamsPerPackets = qtySheets/500;
+
+	//get price w/o wastage
+	//price w/o wastage = qty reams per packets x pricing per ream
+	var priceWithoutWastage = qtyReamsPerPackets * paperPricingPerReam;
+
+	//get wastage
+	//wastage = price w/o wastage x 0.2
+	var wastage = priceWithoutWastage * 0.2;
+
+	//get price with wastage
+	//price with wastage = price w/o wastage + wastage
+	return priceWithoutWastage + wastage;
+
+}
+
 function calculateFlyerPrice(){
 
 	//clear existing html
 	$('#flyer-total-price').html('');
 
-	//get paper cuts from paper size
-	var paperCuts = 0;
-	if( getFlyerPaperSize() == "A4" ){
-		paperCuts = 8;
-	}else if( getFlyerPaperSize() == "A5" ){
-		paperCuts = 16;
-	}
-
-	//insert codes for paper type here
-
-	//get paper side cost
-	var paperSideCost = 0;
-	if( getFlyerPaperSide() == "oneside" ){
-		paperSideCost = 0.5;
-	}else if( getFlyerPaperSide()  == "twosides" ){
-		paperSideCost = 0.7;
-	}
-
-	//get printer from quantity
-	// 0 -> Digital
-	// 1 -> Not Digital
-	var printer = 0;
-	if ( getFlyerQuantity() > 0 ) {
-
-		if ( getFlyerQuantity() > 300 ) {
-			//if qty is more than 300
-			printer = 1;
-		}
-
-	}else{
-		//insert codes handling 0 qty here
-	}
-
-	//calculate price
-	var totalPrice = 0;
-	if ( printer == 0 ) {
-		//if printer is digital
-		totalPrice = paperSideCost * getFlyerQuantity();
-
-	}else if( printer == 1 ){
-		//if printer is not digital
-
-	}else{
-		//insert codes handling other values of printer here
-	}
-
 	//append new html
 	$('#flyer-total-price').append(
-		"<h3> Paper Size: " + getFlyerPaperSize() + "</h3>" +
-		"<h3> Paper Type: " + getFlyerPaperType() + "</h3>" +
-		"<h3> Paper Side: " + getFlyerPaperSide() + "</h3>" +
-		"<h3> Quantity: " + getFlyerQuantity() + "</h3>" +
-		"<h3> Total Price: $" + totalPrice + "</h3>"
+		"<h3> Total Cost of Materials: $" + getMaterialsCost() + "</h3>"
 	);
 }
